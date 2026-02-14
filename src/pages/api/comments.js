e997b6347fb64d458a92151dde9dc6a5693d9739
luisduranpe@gmail.com <tuemail@gmail.com>
2026-02-13 20:00:44 -0700

    add side comments layout

--- src/pages/api/comments.js
+++ src/pages/api/comments.js
@@ -1,4 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
+console.log("URL:", process.env.SUPABASE_URL);
+console.log("KEY:", process.env.SUPABASE_ANON_KEY?.slice(0,20));
 
 const supabase = createClient(
   process.env.SUPABASE_URL,
